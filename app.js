f654d350541eec44a46f78308f16af462a3100f5
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-03-26 13:39:40 +0700

    change app.js2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var indexxRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
